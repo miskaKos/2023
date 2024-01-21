25b58ea13aa8fc95095124a362910ef8c83984c7
miskaKos <kosova.m@outlook.cz>
2024-01-21 18:08:20 +0100

    nedela 18hod

--- election_scraper/pokusy.py
+++ election_scraper/pokusy.py
@@ -83,71 +83,63 @@ def zapis_data(data: list, jmeno_souboru: str) -> str:
 # print(zapis_csv)
         
 
-for odkaz in range(2): #odkazy:
+for odkaz in range(3): #odkazy:
     r2 = requests.get(odkazy[odkaz])
-    location_soup = bs(r2.text, features="html.parser")
-    cisla_seznam = location_soup.find("table", {"id": "ps311_t1"})
+    try: 
+        location_soup = bs(r2.text, features="html.parser")
+        cisla_seznam = location_soup.find("table", {"id": "ps311_t1"})
+        cisla_seznam_td = cisla_seznam.find_all("td")
+        cisla_seznam_list = []
+        for j in cisla_seznam_td:
+            cisla_seznam_list.append(j.text.strip())
 
 
-# r2 = requests.get(odkaz)
-# location_soup = bs(r2.text, features="html.parser")
-# cisla_seznam = location_soup.find("table", {"id": "ps311_t1"})
-# print(cisla_seznam)
-# # break
-
-    cisla_seznam_td = cisla_seznam.find_all("td")
-    # print(cisla_seznam_td)
-    cisla_seznam_list = []
-    for j in cisla_seznam_td:
-        cisla_seznam_list.append(j.text.strip())
-
-
-    registred = cisla_seznam_list[3]
-    envelopes = cisla_seznam_list[4]
-    valid = cisla_seznam_list[7]
+        registred = int(cisla_seznam_list[3])
+        envelopes = int(cisla_seznam_list[4])
+        valid = int(cisla_seznam_list[7])
 
     # cisla = cisla_seznam.select("td", {"class":"cislo", "headers": "sa2"})
 
-    # print(registred)
-    # print(envelopes)
-    # print(valid)
+        print((registred))
+        print(envelopes)
+        print(valid)
 
-    strany_seznam = location_soup.find("div", {"id": "inner"})
+        strany_seznam = location_soup.find("div", {"id": "inner"})
 
-    platne_hlasy_podklad_1 = strany_seznam.find_all("td", {"class":"cislo", "headers": ("t1sb3")})
-    platne_hlasy_podklad_2 = strany_seznam.find_all("td", {"class":"cislo", "headers": ("t2sb3")})
+        platne_hlasy_podklad_1 = strany_seznam.find_all("td", {"class":"cislo", "headers": ("t1sb3")})
+        platne_hlasy_podklad_2 = strany_seznam.find_all("td", {"class":"cislo", "headers": ("t2sb3")})
+        
+        platne_hlasy_1 = []
+        platne_hlasy_2 = []
+
+        for i in platne_hlasy_podklad_1:
+            platne_hlasy_1.append(int(i.text.strip()))
+
+        for i in platne_hlasy_podklad_2:
+            platne_hlasy_2.append(int(i.text.strip()))
+
+        platne_hlasy = platne_hlasy_1 + platne_hlasy_2
+        print(platne_hlasy)
     
-    platne_hlasy_1 = []
-    platne_hlasy_2 = []
+    except AttributeError:
+        location_soup_okrsky = bs(r2.text, features="html.parser")
+        okrsky_table = location_soup_okrsky.find("table", {"class", "table"})
+        table_odkazy = okrsky_table.select('.cislo a')
+        odkazy_okrsky = ["https://volby.cz/pls/ps2017nss/" + okrsky_table['href'] for okrsky_table in table_odkazy]
+        print(odkazy_okrsky)
 
-    for i in platne_hlasy_podklad_1:
-        platne_hlasy_1.append(i.text.strip())
-
-    for i in platne_hlasy_podklad_2:
-        platne_hlasy_2.append(i.text.strip())
-
-    platne_hlasy = platne_hlasy_1 + platne_hlasy_2
-    print(platne_hlasy)
-
-# print(strany_seznam)
-# strany_seznam_td = strany_seznam.find_all("td")
-# strany_seznam_list = []
-# for k in strany_seznam_td:
-#     if k:
-#         strany_seznam_list.append(k.text.strip())
 
     nazvy_stran_1 = []
     nazvy_stran_2 = []
  
-# Nefunguje --> proc????
-for odkaz in range(1): #odkazy:
-    r2 = requests.get(odkazy[odkaz])
+obsah_seznamu = True
+for odkaz in odkazy: 
+    r2 = requests.get(odkaz)
     nazvy_stran_soup = bs(r2.text, features="html.parser")
     nazvy_stran_seznam = nazvy_stran_soup.find("div", {"id": "inner"})
 
     nazvy_stran_podklad_1 = strany_seznam.find_all("td", {"class":"overflow_name", "headers": "t1sa1"})
     nazvy_stran_podklad_2 = strany_seznam.find_all("td", {"class":"overflow_name", "headers": "t2sa1"})
-
 
     for i in nazvy_stran_podklad_1:
         nazvy_stran_1.append(i.text.strip())
@@ -156,52 +148,12 @@ for odkaz in range(1): #odkazy:
 
     for i in nazvy_stran_podklad_2:
         nazvy_stran_2.append(i.text.strip())
-        
+     
     # print(nazvy_stran_2)
+        
+    if obsah_seznamu:
+        if (len(nazvy_stran_1) > 0) and (len(nazvy_stran_2) > 0):
+            nazvy_stran = nazvy_stran_1 + nazvy_stran_2
+            obsah_seznamu = False
+            print(nazvy_stran)
 
-        nazvy_stran = nazvy_stran_1 + nazvy_stran_2
-        print(nazvy_stran)
-
-    # while i <= len(strany_seznam_list):
-    #     nazvy_stran.append(strany_seznam_list[i])
-    #     i = i + 5
-    # print(nazvy_stran)
-    
-
-
-    # j = 2                
-    # while j <= len(strany_seznam_list):
-    #     platne_hlasy.append(strany_seznam_list[j])
-    #     j = j + 5
-    # print(platne_hlasy)
-
-
-# vsechny_tr = obec_seznam.find_all("tr")
-# for tr in vsechny_tr[1:]:
-#     td_na_radku = tr.find_all("td")
-    # print((td_na_radku))
-
-# list = []
-# for i in vsechny_tr:
-#     if i:
-#         list.append(i.text.strip())
-
-# print(list)
-
-# list = [r.replace("\n", " ") for r in list]
-
-# x = ", ".join(list)
-# y = x.split()
-# print((y))
-
-# nazvy_obci = []
-# cisla_obci = []
-# for polozka in y:
-#     if polozka.isdigit():
-#         cisla_obci.append(polozka)
-# for polozka in y:
-#     if polozka.istitle() :
-#         nazvy_obci.append(polozka)
-
-# print(cisla_obci)
-# print(nazvy_obci)
